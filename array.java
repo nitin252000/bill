8cb44f5678cf2a3590f407b1fe14da72e4b7440b
nitin252000 <68811769+nitin252000@users.noreply.github.com>
2020-09-08 21:48:21 +0530

    Add files via upload

---
+++ array.java
@@ -0,0 +1,20 @@
+import java.util.Arrays;
+        import java.util.Scanner;
+//reverse of array
+public class array {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int k=sc.nextInt();
+        int[] n= new int[k];
+        for (int i = 0; i < n.length; i++) {
+            n[i] = sc.nextInt();
+        }
+        System.out.println(Arrays.toString(n));
+        for (int i = 0; i < n.length/2; i++) {
+            int c = n[i];
+            n[i] = n[n.length - i - 1];
+            n[n.length - i - 1] = c;
+        }
+        System.out.println(Arrays.toString(n));
+    }
+}
